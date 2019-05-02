f8fe97608059b2f1effe204a913e7e2cd6503599
Donguk <kdw8573@naver.com>
2019-05-03 01:06:09 +0900

    add Left_sidebar version 1.0

---
+++ front/src/components/molecules/Left_sidebar/index.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import { Left_sidebar } from 'components'
+
+storiesOf('Left_sidebar', module)
+  .add('default', () => (
+    <Left_sidebar>Hello</Left_sidebar>
+  ))
+  .add('reverse', () => (
+    <Left_sidebar reverse>Hello</Left_sidebar>
+  ))
---
+++ front/src/components/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { PageTemplate } from 'components'
+import Left_sidebar from '../../molecules/Left_sidebar'
+
+const Home = () => {
+  return (
+    <Left_sidebar />
+  )
+}
+
+export default Home
