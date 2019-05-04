b4da0ba9b9cc5d03d71227f537f15b6fab2e6fa7
Jongmin <sdrjseka96@naver.com>
2019-05-04 20:25:46 +0900

    5/4 done for model

--- back/snu_moyeo/models.py
+++ back/snu_moyeo/models.py
@@ -7,20 +7,24 @@ from rest_framework.authtoken.models import Token
 
 class Meeting (models.Model):
     id = models.AutoField(primary_key = True)
-    title = models.CharField(max_length=100,blank =True, default='')
-    created = models.DateTimeField(auto_now_add=True) #added variable
+    title = models.CharField(max_length = 100, blank = True, default = '')
+    created = models.DateTimeField(auto_now_add = True) # added variable
     due = models.DateTimeField()
-    min_people = models.IntegerField(default = 1) #modified name
-    max_people = models.IntegerField(default = 999) #modified name
-    
+    min_people = models.IntegerField(default = 1) # modified name
+    max_people = models.IntegerField(default = 999) # modified name
+
     state = models.IntegerField(default = 0)
     description = models.TextField()
-    kind = models.IntegerField(); #need to specify
-    leader =  models.CharField(max_length=30)
+    kind = models.IntegerField(); # need to specify
+    leader = models.ForeignKey('SnuUser', related_name = 'lead_meeting', on_delete=models.CASCADE)
+    members = models.ManyToManyField('SnuUser', through = 'Participate')
 
     class Meta:
         ordering = ['-created']  #order by descending time created
-    
+
+    def save(self, *args, **kwargs) :
+        super(Meeting, self).save(*args, **kwargs)
+
 class SnuUser (AbstractUser):
     # username(unique=True) : ID
     # password
@@ -28,15 +32,21 @@ class SnuUser (AbstractUser):
     # email
     point = models.IntegerField(default = 0)
     mySNU_verified = models.BooleanField(default = False)
-    mySNU_verification_token = models.CharField(max_length=100, unique = True, blank=True)
-#    meetings = models.ManyToManyField(Meeting)
+    mySNU_verification_token = models.CharField(max_length = 100, unique = True, blank = True)
+
+    meetings = models.ManyToManyField('Meeting', through = 'Participate')
 
     # user_id = models.CharField(max_length= 100, primary_key=True) #changed variable name
-    # snu_mail = models.EmailField(default = '') #has inner validator(checker) whether it is valid email address -> EmailValidator with error: ValidationError, "ENter a valid email address" message will be out
+    # snu_mail = models.EmailField(default = '') 
+    # has inner validator(checker) whether it is valid email address 
+    # -> EmailValidator with error: ValidationError, "ENter a valid email address" message will be out
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
+@receiver(post_save, sender = settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance = None, created = False, **kwargs):
     if created:
-        token = Token.objects.create(user=instance)
+        token = Token.objects.create(user = instance)
 
-
+class Participate (models.Model):
+    id = models.AutoField(primary_key = True)
+    user_id = models.ForeignKey('SnuUser', on_delete = models.CASCADE)
+    meeting_id = models.ForeignKey('Meeting', on_delete = models.CASCADE)
--- back/snu_moyeo/views.py
+++ back/snu_moyeo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from snu_moyeo.serializers import MeetingSerializer, SnuUserSerializer
+from snu_moyeo.serializers import MeetingSerializer, SnuUserSerializer, ParticipateSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
-from snu_moyeo.models import SnuUser,Meeting
+from snu_moyeo.models import SnuUser, Meeting, Participate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,9 +12,7 @@ from rest_framework.authtoken.models import Token
 from django.utils.crypto import get_random_string
 from rest_framework import permissions
 from django.http import HttpResponse
-
-def valid_id(username):
-    return '@' not in username
+from snu_moyeo.permissions import UserOnlyAccess, LeaderOnlyControl
 
 class Authenticate (APIView):
     queryset = SnuUser.objects.all()
@@ -24,21 +22,21 @@ class Authenticate (APIView):
     def get(self, request, mySNU_verification_token, format = None):
         try:
             user = SnuUser.objects.get(mySNU_verification_token = mySNU_verification_token)
-        except SnuUser.DoesNotExst:
+        except SnuUser.DoesNotExist:
             return Response(status = status.HTTP_404_NOT_FOUND)
 
         if user.mySNU_verified:
             return Response({'details': 'Already verified'}, status = status.HTTP_400_BAD_REQUEST)
 
         print ('authenticating...')
-        serializer = SnuUserSerializer(user, data={'mySNU_verified': True}, partial=True)
+        serializer = SnuUserSerializer(user, data={'email':user.email, 'mySNU_verified':True}, partial=True)
 
         if serializer.is_valid():
             serializer.save()
             id = serializer.data['id']
             username = serializer.data['username']
             verified = serializer.data['mySNU_verified']
-            return HttpResponse('welcome! '+username+'s mail is verified!',  status=202)
+            return HttpResponse("welcome! "+username+"'s mail is verified!", status=202)
         else :
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
@@ -46,19 +44,19 @@ class SignUp(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPI
     queryset =  SnuUser.objects.all()
     serializer_class = SnuUserSerializer
 
-    # later will must be deleted
+    # It will have to be deleted later
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
     def post(self, request, format=None):
-        username = request.data['username']
+        email = request.data['email']
         serializer = SnuUserSerializer(data=request.data)
 
         if serializer.is_valid():
-            token = get_random_string(length=32)
+            token = get_random_string(length = 32)
             link = "http://127.0.0.1:8000/auth/" + token + "/"
-            send_mail('SnuMoyeo Authenticate', link, 'toro.8906@gmail.com', ['sdrjseka96@snu.ac.kr'], fail_silently=False)
-            serializer.save(mySNU_verification_token = token, mySNU_verified=False)
+            send_mail('SnuMoyeo Authenticate', link, 'toro.8906@gmail.com', [request.data['email']], fail_silently = False)
+            serializer.save(mySNU_verification_token = token, mySNU_verified = False)
             id =  serializer.data['id']
             username = serializer.data['username']
             verified = serializer.data['mySNU_verified']
@@ -69,24 +67,42 @@ class SignUp(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPI
 class LogIn(APIView):
         queryset = SnuUser.objects.all()
         serializer_class = SnuUserSerializer
-        permission_classes = (permissions.IsAuthenticated,)
+        permission_classes = ()
 
-        def get(self, request, format=None):
+        def get(self, request, format = None):
                 print('log in..')
                 user = request.user
                 if user.mySNU_verified == True:
-                    return Response(data = {'username': user.username}, status=status.HTTP_202_ACCEPTED)
+                    return Response(data = {'username':user.username}, status = status.HTTP_202_ACCEPTED)
                 else :
-                    return Response(data = {'details' : 'Not SNU verified.'}, status=status.HTTP_403_FORBIDDEN)
+                    return Response(data = {'details':'Not SNU verified.'}, status = status.HTTP_403_FORBIDDEN)
 
 class MeetingList(generics.ListCreateAPIView):
     queryset = Meeting.objects.all()
     serializer_class = MeetingSerializer
-    permission_classes = ()
-    # def perform_create(self,serializer):
-    #    serializer.save(leader=self.request.name)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    def perform_create(self, serializer):
+        serializer.save(leader = self.request.user)
 
 class MeetingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Meeting.objects.all()
     serializer_class = MeetingSerializer
-    permission_classes = ()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, LeaderOnlyControl,)
+
+class SnuUserList(generics.ListAPIView):
+    queryset = SnuUser.objects.all()
+    serializer_class = SnuUserSerializer
+
+class SnuUserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SnuUser.objects.all()
+    serializer_class = SnuUserSerializer
+    permission_classes = (UserOnlyAccess,)
+
+class ParticipateList(generics.ListCreateAPIView):
+    queryset = Participate.objects.all()
+    serializer_class = ParticipateSerializer
+
+class ParticipateDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Participate.objects.all()
+    serializer_class = ParticipateSerializer
+
