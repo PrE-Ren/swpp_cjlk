07446e87597e642a8abe1b94167b33861fb6bdc7
hjcdg1 <hjcdg197@gmail.com>
2019-05-19 17:37:32 +0900

    List page completed 2

--- front/src/components/organisms/ListPage/index.js
+++ front/src/components/organisms/ListPage/index.js
@@ -49,6 +49,7 @@ export const ListPage = ({ token, meetinglist_list, change_page_num_click }) => 
     const page_state = JSON.parse(meetinglist_list).links
     console.log("확인")
     console.log(page_state)
+      console.log(meetinglist_list)
     const prev_url = page_state.previous
     const next_url = page_state.next
     let prev_page_num, next_page_num, current_page_num
@@ -61,7 +62,6 @@ export const ListPage = ({ token, meetinglist_list, change_page_num_click }) => 
       next_page_num = 0
       prev_page_num = Number(prev_url.split('?page=')[1])
       prev_page_num = isNaN(prev_page_num) ? 1 : prev_page_num
-      console.log(typeof prev_page_num)
       current_page_num = prev_page_num + 1
 
     }
@@ -94,7 +94,10 @@ export const ListPage = ({ token, meetinglist_list, change_page_num_click }) => 
             <KindList /><br />
             <Button type = "submit" onClick={() => change_page_num_click(prev_page_num)}>Prev {prev_text}</Button>&ensp;
             <Button type = "submit" onClick={() => change_page_num_click(current_page_num)}>Current {current_text}</Button>&ensp;
-            <Button type = "submit" onClick={() => change_page_num_click(next_page_num)}>Next {next_text}</Button>
+            <Button type = "submit" onClick={() => change_page_num_click(next_page_num)}>Next {next_text}</Button>&ensp;&ensp;
+            {/* number_of_pages = Math.ceil(JSON.parse(meetinglist_list).count / JSON.parse(meetinglist_list).page_size) */}
+            {/* Can go to the page I want by typing the page number into <input> tag and pass the value to change_page_num_click function */}
+            {/* But it is necessary to check whether it is in the range of [1, number_of_pages] */}
           </List_Box>
           <Right_sidebar />
         </Lower_Box>
