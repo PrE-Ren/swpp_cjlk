853bf5e7e47757458d1ef557d62606eb8e0f48ce
hjcdg1 <hjcdg197@gmail.com>
2019-05-28 02:15:39 +0900

    meeting entry modi

--- front/src/components/atoms/MeetingEntry/index.js
+++ front/src/components/atoms/MeetingEntry/index.js
@@ -21,7 +21,10 @@ export class MeetingEntry extends React.Component {
     let meeting_entry = (
       <Card.Content style={{ cursor: 'pointer' }} onClick={() => { this.props.load_comments_click(this.props.meeting_info.id) }}>
         <Card.Header as='h5'>{this.props.meeting_info.title}</Card.Header>
-        <Card.Meta>모집 현황 : {this.props.meeting_info.members.length}/{this.props.meeting_info.max_people}</Card.Meta>
+        <Card.Meta>
+          모집 현황 : {this.props.meeting_info.members.length}명/{this.props.meeting_info.max_people}명&ensp;
+          (최소 인원 : {this.props.meeting_info.min_people}명)
+        </Card.Meta>
         <Card.Meta>마감 기한 : {dateParse(this.props.meeting_info.due)}</Card.Meta>
         <Card.Description>{this.props.meeting_info.description}</Card.Description>
       </Card.Content>
