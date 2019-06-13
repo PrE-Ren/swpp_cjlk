5199a7cac991b24ecd1009c3589b642fcf60e119
hjcdg1 <hjcdg197@gmail.com>
2019-06-14 05:04:08 +0900

    recent, impending, join, lead, history, all, kind list Trim

--- front/src/components/molecules/AllList/index.js
+++ front/src/components/molecules/AllList/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import MeetingEntry from '../../../containers/MeetingEntry'
 import { Segment, Card, Header, Label } from 'semantic-ui-react'
 
+// meetinglist_all : All 페이지에서 보여줄 미팅 리스트 정보
 export const AllList = ({ meetinglist_all }) => {
   console.log('<AllList Rendering>')
   if (meetinglist_all != null) {
-    console.log(meetinglist_all)
-    let meetings = JSON.parse(meetinglist_all).results
+    let meetings = JSON.parse(meetinglist_all).results  //  미팅 리스트
     return (
       <Segment raised>
-        <Label as='a' color='pink' ribbon>All</Label>
-        <Header as='h1'>전체 검색 결과</Header>
-        <Card.Group>
+        <Label as='a' color='pink' ribbon> All </Label>  {/* 라벨 */}
+        <Header as='h1'> 전체 검색 결과 </Header>         {/* 제목 */}
+        <Card.Group>                                     {/* 각각의 카드는 하나의 미팅 엔트리를 담고 있음 */}
           {meetings.map(meeting_entry =>
             <Card key = {meeting_entry.id} >
               <MeetingEntry meeting_info = {meeting_entry}/>
@@ -22,5 +22,5 @@ export const AllList = ({ meetinglist_all }) => {
     )
   }
   else
-    return <div></div>
+    return (<div></div>)
 }
