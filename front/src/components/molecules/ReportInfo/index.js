a20c39d099f1d532380ab7bc93e06cb6cc22dbd3
Donguk <kdw8573@naver.com>
2019-06-17 21:14:45 +0900

    fixed adminPage

--- front/src/components/molecules/ReportInfo/index.js
+++ front/src/components/molecules/ReportInfo/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Segment, Form, Grid, Card, Button } from 'semantic-ui-react'
-
+import styled from 'styled-components'
 // username : 유저 아이디
 // password : 유저 패스워드
 // report_info : id, created, reason, isHandled, point, reporter, reporterid, reportee, reporteeid
+
+const EllipsisCss = styled.span`
+  display: inline-block;
+  width: 190px;
+  overflow: hidden;
+`
+
 export class ReportInfo extends React.Component {
   constructor(props) {
     super(props);
@@ -17,33 +24,31 @@ export class ReportInfo extends React.Component {
       <Card.Content>
 
         {/* 신고 기본 정보  */}
-        <Card.Meta> 신고자 : {this.props.report_info.reporter} ▶ 신고 대상 : {this.props.report_info.reportee} </Card.Meta>
-        <Card.Meta> 신고 사유 : {this.props.report_info.reason} </Card.Meta>
+        <Card.Description>
+          신고자 : {this.props.report_info.reporter} ▶ 신고 대상 : {this.props.report_info.reportee}<br/><br/>
+          <EllipsisCss>신고 사유 : {this.props.report_info.reason}</EllipsisCss>
+        </Card.Description>
         <p></p>
 
         {/* 신고 처리 부분 (부여 벌점 입력, 처리 혹은 기각)*/}
         <Form size='large'>
           {!this.props.report_info.isHandled
           ?
-          <Segment stacked>  {/* 처리되지 않은 신고 */}
-            <Form.Input fluid icon='user' iconPosition='left' placeholder='부여할 벌점 점수' onChange={(e) => point = e.target.value}/>
-            <Grid columns={2}>
-              <Grid.Column width={8}>
-                <Button color='teal' fluid size='large' onClick={() => this.props.penalty_click(hash, true, this.props.report_info, point)}>처리</Button>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <Button color='teal' fluid size='large' onClick={() => this.props.penalty_click(hash, true, this.props.report_info, 0)}>기각</Button>
-              </Grid.Column>
-            </Grid>
-          </Segment>
+          <Card.Content extra>
+            <Form.Input fluid icon='exclamation triangle' iconPosition='left' placeholder='부여할 벌점 점수' onChange={(e) => point = e.target.value}/>
+            <div className='ui two buttons'>
+              <Button basic color='green' fluid size='large' onClick={() => this.props.penalty_click(hash, true, this.props.report_info, point)}>처리</Button>
+              <Button basic color='red' fluid size='large' onClick={() => this.props.penalty_click(hash, true, this.props.report_info, 0)}>기각</Button>
+            </div>
+          </Card.Content>
           :
-          <Segment stacked>  {/* 처리된 신고 */}
-            <Grid columns={1}>
-              <Grid.Column width={16}>
-                <Button color='teal' fluid size='large' onClick={() => this.props.penalty_click(hash, false, this.props.report_info, 0)}>되돌리기</Button>
-              </Grid.Column>
-            </Grid>
-          </Segment>}
+          <Card.Content extra>
+            <br/>
+            <Card.Meta>이미 처리된 신고 입니다.</Card.Meta>
+            <br/>
+            <Button basic color='green' fluid size='large' onClick={() => this.props.penalty_click(hash, false, this.props.report_info, 0)}>되돌리기</Button>
+          </Card.Content>
+          }
         </Form>
 
       </Card.Content>
