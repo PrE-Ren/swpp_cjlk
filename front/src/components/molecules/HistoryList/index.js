4bd961a36beebc3ca57e99d1f48c9c42f3298c0a
Donguk <kdw8573@naver.com>
2019-06-17 18:57:05 +0900

    Mypage fold/unfold

--- front/src/components/molecules/HistoryList/index.js
+++ front/src/components/molecules/HistoryList/index.js
@@ -1,26 +1,46 @@
 import React from 'react'
 import MeetingEntry from '../../../containers/MeetingEntry'
-import { Segment, Card, Header, Label } from 'semantic-ui-react'
+import { Segment, Card, Header, Label, Button } from 'semantic-ui-react'
 
-// meetinglist_history : MyPage 페이지에서 보여줄 History 리스트 정보
-export const HistoryList = ({ meetinglist_history }) => {
-  console.log('<History Rendering>')
-  if (meetinglist_history != null) {
-    let meetings = JSON.parse(meetinglist_history)  //  미팅 리스트
-    return (
-      <Segment raised>
-        <Label as='a' color='red' ribbon>History</Label> {/* 라벨 */}
-        <Header as='h1'>내가 참여했던 모임</Header>        {/* 제목 */}
-        <Card.Group>                                        {/* 각각의 카드는 하나의 미팅 엔트리를 담고 있음 */}
-          {meetings.map(meeting_entry =>
-            <Card key = {meeting_entry.id} >
-              <MeetingEntry meeting_info = {meeting_entry}/>
-            </Card>
-          )}
-        </Card.Group>
-      </Segment>
-    )
+
+export class HistoryList extends React.Component {
+  state = {
+    fold_click : false
   }
-  else
-    return (<div></div>)
+
+  list_show = () => this.setState({ fold_click : true })
+  list_close = () => this.setState({ fold_click : false})
+
+  render() {
+    const { meetinglist_history } = this.props
+    console.log('<History Rendering>')
+    if (meetinglist_history != null) {
+      let meetings = JSON.parse(meetinglist_history)  //  미팅 리스트
+      return (
+        <Segment raised>
+          <Label as='a' color='red' ribbon>History</Label> {/* 라벨 */}
+          <Header as='h1'>내가 참여했던 모임 &nbsp;
+          {this.state.fold_click ?
+            <Button basic color='green' circular icon='triangle up' content = '접기' onClick={() => this.list_close()}/>
+            :
+            <Button basic color='green' circular icon='triangle down' content = '펼치기' onClick={() => this.list_show()}/>
+          }</Header>                        {/* 제목 */}
+
+          {this.state.fold_click ?
+            <Card.Group>                                                {/* 각각의 카드는 하나의 미팅 엔트리를 담고 있음 */}
+              {meetings.map(meeting_entry =>
+                <Card key = {meeting_entry.id} >
+                  <MeetingEntry meeting_info = {meeting_entry}/>
+                </Card>
+              )}
+            </Card.Group>
+          :
+          <div></div>
+          }
+        </Segment>
+      )
+    }
+    else
+      return (<div></div>)
+  }
 }
--- front/src/components/molecules/LeadList/index.js
+++ front/src/components/molecules/LeadList/index.js
@@ -1,26 +1,47 @@
 import React from 'react'
 import MeetingEntry from '../../../containers/MeetingEntry'
-import { Segment, Card, Header, Label } from 'semantic-ui-react'
+import { Segment, Card, Header, Label, Button } from 'semantic-ui-react'
 
-// meetinglist_lead : MyPage 페이지에서 보여줄 Lead 리스트 정보
-export const LeadList = ({ meetinglist_lead }) => {
-  console.log('<Lead Rendering>')
-  if (meetinglist_lead != null) {
-    let meetings = JSON.parse(meetinglist_lead)  //  미팅 리스트
-    return (
-      <Segment raised>
-        <Label as='a' color='red' ribbon>I am leader!</Label>  {/* 라벨 */}
-        <Header as='h1'>내가 만든 모임</Header>                    {/* 제목 */}
-        <Card.Group>                                                {/* 각각의 카드는 하나의 미팅 엔트리를 담고 있음 */}
-          {meetings.map(meeting_entry =>
-            <Card key = {meeting_entry.id} >
-              <MeetingEntry meeting_info = {meeting_entry}/>
-            </Card>
-          )}
-        </Card.Group>
-      </Segment>
-    )
+
+export class LeadList extends React.Component {
+  state = {
+    fold_click : false
   }
-  else
-    return (<div></div>)
+
+  list_show = () => this.setState({ fold_click : true })
+  list_close = () => this.setState({ fold_click : false})
+
+  render() {
+    const { meetinglist_lead } = this.props
+    console.log('<Lead Rendering>')
+    if (meetinglist_lead != null) {
+      let meetings = JSON.parse(meetinglist_lead)  //  미팅 리스트
+      return (
+        <Segment raised>
+          <Label as='a' color='red' ribbon>I am leader!</Label>  {/* 라벨 */}
+          <Header as='h1'>내가 만든 모임 &nbsp;
+          {this.state.fold_click ?
+            <Button basic color='green' circular icon='triangle up' content = '접기' onClick={() => this.list_close()}/>
+            :
+            <Button basic color='green' circular icon='triangle down' content = '펼치기' onClick={() => this.list_show()}/>
+          }</Header>                    {/* 제목 */}
+
+
+          {this.state.fold_click ?
+            <Card.Group>                                                {/* 각각의 카드는 하나의 미팅 엔트리를 담고 있음 */}
+              {meetings.map(meeting_entry =>
+                <Card key = {meeting_entry.id} >
+                  <MeetingEntry meeting_info = {meeting_entry}/>
+                </Card>
+              )}
+            </Card.Group>
+          :
+          <div></div>
+          }
+        </Segment>
+      )
+    }
+    else
+      return (<div></div>)
+  }
 }
